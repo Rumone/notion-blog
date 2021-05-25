7348efd46b2b91c224718d0da5bc45e049840385
rumoneanderson <rumoneanderson245@gmail.com>
2021-05-24 23:57:34 -0500

    testing notion api setup

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { config } from "dotenv";
+import { Client } from "@notionhq/client";
+import { BlocksChildrenListResponse, DatabasesQueryResponse } from "@notionhq/client/build/src/api-endpoints";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const notion = new Client({ auth: process.env.NOTION_KEY })
+const db_id = process.env.NOTION_DATABASE_ID
+
+export default async (req, res) => {
+
+  const page_result: DatabasesQueryResponse = await notion.databases.query({
+    database_id: db_id,
+  })
+
+  let page_id: string;
+
+  if (page_result.results[0].object === "page") {
+    page_id = page_result.results[0].id
+  } else {
+    console.log("Not page")
+  }
+
+  const page_blocks: BlocksChildrenListResponse = await notion.blocks.children.list({
+    block_id: page_id,
+    page_size: 100
+  })
+
+
+  console.log(page_blocks)
+
+  res.status(200).json(page_blocks)
 }
