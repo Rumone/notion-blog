dfc651b3a63073706a3b3602588fcce58e321a63
Rumone <63555633+Rumone@users.noreply.github.com>
2021-06-03 08:53:29 -0500

    Revert "Initial GraphQl API implementation (POC)"

--- utils/api/resolvers.ts
+++ utils/api/resolvers.ts
@@ -1,19 +1,5 @@
-import { getDatabasePages } from "../notion";
-
-const tempDb = {
-    firstname: "josh",
-    lastname: "brown",
-    email: "joshbrown@gmail.com"
-}
-
-
-const tempResolve = (param: string) => tempDb[param];
-
-
 export const resolvers = {
     Query: {
-        articles: async (parent, args, context) => {
-            return await getDatabasePages()
-        }
-    },
+        hello: (parent, args, context) => "Hello"
+    }
 }
