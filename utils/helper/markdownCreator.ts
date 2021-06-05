0340bd130f7470ea3c42467ceda7f7e3e4b1d911
rumoneanderson <rumoneanderson245@gmail.com>
2021-06-04 19:52:29 -0500

    update to markdown generator function for annotations

--- utils/helper/markdownCreator.ts
+++ utils/helper/markdownCreator.ts
@@ -1,12 +1,41 @@
 import { RichText } from "@notionhq/client/build/src/api-types";
 import { BlockType } from "../types";
 
+enum AnnotationHelper {
+    bold = "bold",
+    italic = "italic",
+    strikethrough = "strikethrough",
+    underline = "underline",
+    // NOTE: code not currently supported in Notion API
+    code = "code",
+    defaultColor = "default"
+}
+
+
 export const textToMarkdownConverter = (blockType: string, blockText: RichText[]): string => {
     let content = "";
     blockText.forEach(textElement => {
-        content += textElement.plain_text;
+
+        let currentTextElement = textElement.plain_text;
+        if (textElement.annotations[AnnotationHelper.bold]) {
+            currentTextElement = `**${currentTextElement}**`
+        }
+        if (textElement.annotations[AnnotationHelper.italic]) {
+            currentTextElement = `_${currentTextElement}_`
+        }
+        if (textElement.annotations[AnnotationHelper.strikethrough]) {
+            currentTextElement = `~~${currentTextElement}~~`
+        }
+        if (textElement.annotations[AnnotationHelper.underline]) {
+            currentTextElement = `<u>${currentTextElement}</u>`
+        }
+        if (textElement.annotations.color !== AnnotationHelper.defaultColor) {
+            currentTextElement = `<span style="color:${textElement.annotations.color}">${currentTextElement}</span>`
+        }
+        content += currentTextElement;
     })
 
+    // TODO: Complete implementations for other Block types
     switch (blockType) {
         case BlockType.heading_1:
             content = createMardownHeadingOne(content);
